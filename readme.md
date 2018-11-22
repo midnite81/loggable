72eb0e0e868839d45909669fa5dc68d3d1badba2
Simon Rogers <webdev@centralblue.co.uk>
2018-11-22 23:33:44 +0000

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Loggable [![Latest Stable Version](https://poser.pugx.org/midnite81/loggable/version)](https://packagist.org/packages/midnite81/loggable) [![Total Downloads](https://poser.pugx.org/midnite81/loggable/downloads)](https://packagist.org/packages/midnite81/loggable) [![Latest Unstable Version](https://poser.pugx.org/midnite81/loggable/v/unstable)](https://packagist.org/packages/midnite81/loggable) [![License](https://poser.pugx.org/midnite81/loggable/license.svg)](https://packagist.org/packages/midnite81/loggable) [![Build](https://travis-ci.org/midnite81/loggable.svg?branch=master)](https://travis-ci.org/midnite81/loggable) [![Coverage Status](https://coveralls.io/repos/github/midnite81/loggable/badge.svg?branch=master)](https://coveralls.io/github/midnite81/loggable?branch=master)
+_This is a small package to make logging to a file more of a doddle_
+
+
+**Documentation to follow - work in progress**
---
+++ src/BaseLogger.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Midnite81\Loggable;
+
+use Carbon\Carbon;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+abstract class BaseLogger
+{
+    /** @var string */
+    protected $directoryPath;
+
+    /** @var string */
+    protected $filePrefix;
+
+    /** @var \Monolog\Logger */
+    protected $logger;
+
+    /** @var StreamHandler */
+    protected $streamHandler;
+
+    /** @var Formatter */
+    protected $formatter;
+
+    /**
+     * Get Directory Path
+     *
+     * @return string
+     */
+
+    /**
+     * Return the path to where the log file will be stored
+     *
+     * @return string
+     */
+    abstract public function getDirectoryPath();
+
+    /**
+     * Returns the prefix to the log file.
+     * All files have the date appended to them
+     *
+     * @return string
+     */
+    abstract public function getFilePrefix();
+
+    /**
+     * Returns the extension of the log file.
+     * If null, will default to 'log'
+     *
+     * @return string
+     */
+    abstract public function getExtension();
+
+    /**
+     * Return the name for the logger
+     *
+     * @return string
+     */
+    abstract public function getLoggerName();
+
+    /**
+     * Get the maximum number of days a log file can exist for
+     *
+     * @return int
+     */
+    public function getMaxLogDays() {
+        return 5;
+    }
+
+    /**
+     * Return the logger
+     * (This function can be overwritten on your child class)
+     *
+     * @return Logger
+     * @throws \Exception
+     */
+    public function getLogger() {
+        $directory = $this->getDirectoryPath();
+        $file = $this->getFilePrefix() . '-' . date('Y-m-d') . '.' . $this->getExtension();
+        $filename = $directory . DIRECTORY_SEPARATOR . $file;
+
+        $log = new Logger($this->getLoggerName());
+        $formatter = new LineFormatter(null, null, false, true);
+        $stream = new StreamHandler($filename);
+        $stream->setFormatter($formatter);
+        $log->pushHandler($stream);
+
+        return $log;
+    }
+
+
+    /**
+     * Clear older files so we don't fill the server with log files
+     */
+    public function clearOldLogs()
+    {
+        $files = glob($this->getDirectoryPath() . DIRECTORY_SEPARATOR . '*.' . $this->getExtension());
+        $allowableMaxAge = Carbon::now()->startOfDay()->subDay($this->getMaxLogDays());
+
+        if ($files) {
+            foreach ($files as $file) {
+                $fileDate = str_replace([$this->getFilePrefix() . '-', '.' . $this->getExtension()], "", basename($file));
+                if (Carbon::createFromFormat('Y-m-d', $fileDate)->lessThan($allowableMaxAge)) {
+                    unlink($file);
+                }
+            }
+        }
+    }
+
+    /**
+     * Call Static Magic Method
+     *
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        $class = new static;
+
+        $log = $class->getLogger();
+
+        return $log->{$name}($arguments);
+    }
+
+    /**
+     * Call Magic Method
+     *
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call($name, $arguments)
+    {
+        $log = $this->getLogger();
+
+        return $log->{$name}($arguments);
+    }
+}
